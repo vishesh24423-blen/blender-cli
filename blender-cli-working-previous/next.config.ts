7eaf9d9308f12eba4cb1e39cd895c0e46373f694
Vishesh2445 <visheshsingh7655@gmail.com>
2026-04-11 22:57:37 +0530

    fix4

--- blender-cli-working-previous/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-
-  serverExternalPackages: ['firebase-admin'],
-};
-
-export default nextConfig;
