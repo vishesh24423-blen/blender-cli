1c84809497a1ac12477d265c3b39304aeb6f6f06
Marc <marc@trademarktc.com>
2026-06-11 10:17:28 +0000

    Simplify default sample script

--- components/ScriptSubmitForm.tsx
+++ components/ScriptSubmitForm.tsx
@@ -14,24 +14,16 @@ const FORMATS: { id: OutputFormat; label: string }[] = [
 
 const SAMPLE_SCRIPT = `import bpy
 
-# Create a cube
+# Create a smooth cube with material
 bpy.ops.mesh.primitive_cube_add(size=2, location=(0, 0, 0))
 cube = bpy.context.active_object
 cube.name = "MyCube"
-
-# Subdivide for smoother appearance
-modifier = cube.modifiers.new("Subdivision", 'SUBSURF')
-modifier.levels = 2
-
-# Apply smooth shading
 bpy.ops.object.shade_smooth()
 
-# Add a simple material
-mat = bpy.data.materials.new("CubeMaterial")
+mat = bpy.data.materials.new("MyMaterial")
 mat.use_nodes = True
 bsdf = mat.node_tree.nodes['Principled BSDF']
 bsdf.inputs['Base Color'].default_value = (0.1, 0.5, 0.9, 1.0)
-bsdf.inputs['Roughness'].default_value = 0.5
 cube.data.materials.append(mat)
 `;
 
